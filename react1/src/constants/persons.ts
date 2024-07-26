bb91846e147c6412d6dc5c576176c37b4f6921ab
sravanimamidgi_ldp <sravani.mamidigi@zemosolabs.com>
2024-07-26 13:10:38 +0530

    addded InputDisplay and ActivePerson

---
+++ react1/src/constants/persons.ts
@@ -0,0 +1,29 @@
+const persons = [
+   {
+      id: 1,
+      name: 'sravani',
+      age: 20,
+      isActive: true,
+    },
+    {
+      id: 2,
+      name: 'yamini',
+      age: 20,
+      isActive: true,
+    },
+    {
+      id: 3,
+      name: 'glory',
+      age: 21,
+      isActive: false,
+    },
+    {
+      id: 4,
+      name: 'priya',
+      age: 21,
+      isActive: false,
+    },
+  ];
+  
+  export default persons;
+  
